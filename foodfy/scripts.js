167ceb065cde89d41f2aca5fe98d960288f2c79a
Dhiego Gomes <59400458+dhiego-gomes@users.noreply.github.com>
2020-10-29 17:28:15 -0300

    Delete scripts.js

--- foodfy/scripts.js
+++
@@ -1,7 +0,0 @@
-const menus = document.querySelectorAll('.menu__link')
-
-for (var i = 0; i < menus.length; i++) {
-    if (menus[i].href == document.URL) {
-        menus[i].classList.add('clicked');
-    }
-}
